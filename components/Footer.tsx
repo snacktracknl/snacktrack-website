a03a884c2f8e1d26635de254c9ac1adbd06b3a3f
snacktracknl <info@snacktrack.nl>
2025-04-24 16:56:51 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-white-100 text-center p-4 mt-10">
+    <footer className="bg-white-100 text-center p-4 mt-auto">
       <p>&copy; 2025 SnackTrack</p>
     </footer>
   )
