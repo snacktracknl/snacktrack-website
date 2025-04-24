86ec2e6cd7b93a8efd672c3d6066aafd1818b5fb
snacktracknl <info@snacktrack.nl>
2025-04-24 16:01:28 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <div className="relative h-[60vh] bg-cover bg-center text-white" style={{ backgroundImage: "url('/images/hero.jpg')" }}>
+    <div className="relative h-[75vh] bg-cover bg-center text-white" style={{ backgroundImage: "url('/images/hero.jpg')" }}>
       <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center text-center px-4">
         <Image src="/images/logo.png" alt="SnackTrack logo" width={150} height={150} />
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tight mb-6">SNACK SMARTER.<br />RIDE HARDER.</h1>
