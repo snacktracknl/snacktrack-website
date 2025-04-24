b710c8571a94494839dc07702bcc8b1e2559e00e
snacktracknl <info@snacktrack.nl>
2025-04-24 12:37:59 +0200

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welkom bij SnackTrack</h1>
-      <p>Snack slimmer. Ride harder.</p>
-    </main>
-  );
-}
