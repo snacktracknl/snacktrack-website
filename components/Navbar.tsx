751d8ddc24d1d9404b352b4d777190c5da7b72c9
snacktracknl <info@snacktrack.nl>
2025-04-24 16:40:09 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,30 +6,21 @@ export default function Navbar() {
     <>
       {/* Witte balk met logo */}
       <div className="bg-white shadow py-1.5 z-50 relative flex justify-between items-center" style={{ height: '130px' }}>
-        <div className="flex justify-center w-full absolute left-0 right-0">
-          <Image src="/images/logo.png" alt="SnackTrack logo" width={180} height={180} />
-        </div>
-
-        {/* Navigatie rechts uitgelijnd */}
-        <nav className="mr-6">
-          <ul className="flex gap-6 text-lg font-bold text-[#003366]"> {/* Donkerblauwe tekst */}
-            <li>
-              <Link href="/" className="hover:text-orange-500">Home</Link>
-            </li>
-            <li>
-              <Link href="/over-ons" className="hover:text-orange-500">Over ons</Link>
-            </li>
-            <li>
-              <Link href="/automaten" className="hover:text-orange-500">Automaten</Link>
-            </li>
-            <li>
-              <Link href="/locaties" className="hover:text-orange-500">Locaties</Link>
-            </li>
-            <li>
-              <Link href="/contact" className="hover:text-orange-500">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+          {/* Logo in het midden */}
+          <div className="flex justify-center w-full absolute left-0 right-0">
+              <Image src="/images/logo.png" alt="SnackTrack logo" width={180} height={180} />
+          </div>
+      
+          {/* Navigatie rechts */}
+          <nav className="absolute right-6">
+              <ul className="flex gap-6 text-lg font-bold text-[#003366]">
+                  <li><Link href="/" className="hover:text-orange-500">Home</Link></li>
+                  <li><Link href="/over-ons" className="hover:text-orange-500">Over ons</Link></li>
+                  <li><Link href="/automaten" className="hover:text-orange-500">Automaten</Link></li>
+                  <li><Link href="/locaties" className="hover:text-orange-500">Locaties</Link></li>
+                  <li><Link href="/contact" className="hover:text-orange-500">Contact</Link></li>
+              </ul>
+          </nav>
       </div>
     </>
   )
