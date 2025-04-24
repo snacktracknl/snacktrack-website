3cbf9b49bfc259c9c9deda02eed3741d334a137f
snacktracknl <info@snacktrack.nl>
2025-04-24 12:32:45 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welkom bij SnackTrack</h1>
+      <p>Snack slimmer. Ride harder.</p>
+    </main>
+  );
+}
