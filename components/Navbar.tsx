9893748658a366bc449a76889067578e53bbbdc4
snacktracknl <info@snacktrack.nl>
2025-04-24 16:16:06 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <>
-      {/* Witte balk met logo */}
-      <div className="bg-white shadow py-4 z-50 relative">
+      {/* Compacte witte balk met logo */}
+      <div className="bg-white shadow py-2 z-50 relative">
         <div className="flex justify-center">
           <Image src="/images/logo.png" alt="SnackTrack logo" width={200} height={200} />
         </div>
       </div>
 
-      {/* Navigatie bovenop hero, witte tekst */}
+      {/* Navigatie bovenop hero-afbeelding */}
       <nav className="absolute top-4 left-0 w-full z-40 text-white">
         <ul className="flex justify-center gap-6 text-lg font-bold tracking-wide drop-shadow-lg">
           <li><Link href="/" className="hover:underline">Home</Link></li>
