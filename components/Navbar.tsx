983873850113e0e4a0ec166da37810947276f968
snacktracknl <info@snacktrack.nl>
2025-04-24 16:14:18 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,24 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <header className="absolute top-0 left-0 w-full z-50 text-white">
-      <div className="flex flex-col items-center pt-4">
-        <Image src="/images/logo.png" alt="SnackTrack logo" width={200} height={200} />
-        <nav className="mt-4">
-          <ul className="flex gap-6 text-lg font-extrabold tracking-wide">
-            <li><Link href="/" className="hover:underline">Home</Link></li>
-            <li><Link href="/over-ons" className="hover:underline">Over ons</Link></li>
-            <li><Link href="/automaten" className="hover:underline">Automaten</Link></li>
-            <li><Link href="/locaties" className="hover:underline">Locaties</Link></li>
-            <li><Link href="/contact" className="hover:underline">Contact</Link></li>
-          </ul>
-        </nav>
+    <>
+      {/* Witte balk met logo */}
+      <div className="bg-white shadow py-4 z-50 relative">
+        <div className="flex justify-center">
+          <Image src="/images/logo.png" alt="SnackTrack logo" width={200} height={200} />
+        </div>
       </div>
-    </header>
+
+      {/* Navigatie bovenop hero, witte tekst */}
+      <nav className="absolute top-4 left-0 w-full z-40 text-white">
+        <ul className="flex justify-center gap-6 text-lg font-bold tracking-wide drop-shadow-lg">
+          <li><Link href="/" className="hover:underline">Home</Link></li>
+          <li><Link href="/over-ons" className="hover:underline">Over ons</Link></li>
+          <li><Link href="/automaten" className="hover:underline">Automaten</Link></li>
+          <li><Link href="/locaties" className="hover:underline">Locaties</Link></li>
+          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
+        </ul>
+      </nav>
+    </>
   )
 }
